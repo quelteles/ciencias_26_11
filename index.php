6ee61b96fd5ed3b6e3497ea6e97c5571ae5a1554
raquel <raquelteles9627@gmail.com>
2024-12-06 18:26:27 -0400

    trabalho de casa

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>trabalho de casa</title>
+</head>
+<body>
+<!-- exemplo de syntax -->
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+
+// exemplo de arrays
+$cars = array("Volvo", "BMW", "Toyota");
+
+?>
+</body>
+</html>
